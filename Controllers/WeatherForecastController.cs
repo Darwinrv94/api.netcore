15b78a840ed615b02856a8cce17e50f92cb19719
DESKTOP-FP5J1U6\Darwi <darwinrv94@gmail.com>
2022-06-22 11:01:36 -0400

    Rutas, Middlewares e inyección de dependencias

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace webapi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
@@ -13,20 +13,50 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> _logger;
 
+    private static List<WeatherForecast> ListWeatherForeCast = new List<WeatherForecast>();
+
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+
+        if (ListWeatherForeCast == null || !ListWeatherForeCast.Any())
+        {
+            ListWeatherForeCast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToList();
+        }
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
+    //[Route("Get/GetWeatherForecast")]
+    //[Route("Get/GetWeatherForecast2")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return ListWeatherForeCast;
+    }
+
+    [HttpPost]
+    public IActionResult Post(WeatherForecast weatherForecast)
+    {
+        ListWeatherForeCast.Add(weatherForecast);
+
+        return Ok();
+    }
+
+    [HttpDelete("{index}")]
+    public IActionResult Delete(int index)
+    {
+        if (ListWeatherForeCast.ElementAtOrDefault(index) == null)
         {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            return NotFound(new { message = "WeatherForecast not found" });
+        }
+
+        ListWeatherForeCast.RemoveAt(index);
+
+        return Ok();
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//builder.Services.AddScoped<IHelloWorldService, HelloWorldService>();
+
+builder.Services.AddScoped<IHelloWorldService>(p => new HelloWorldService());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +23,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+//app.UseWelcomePage();
+// app.UseTimeMiddleware();
 
 app.MapControllers();
 
